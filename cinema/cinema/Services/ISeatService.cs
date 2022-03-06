11e832a701e247dd111721750a116e7da6a76997
Jeroen Rijkse <jeroen@rijk.se>
2022-03-06 13:15:16 +0100

    creating service

--- cinema/cinema/Services/ISeatService.cs
+++ cinema/cinema/Services/ISeatService.cs
@@ -1,6 +1,8 @@
+using cinema.Models;
+
 namespace cinema.Services;
 
 public interface ISeatService
 {
-    
+    public Array GetSeats(string show, int quantity);
 }
--- cinema/cinema/Services/SeatService.cs
+++ cinema/cinema/Services/SeatService.cs
@@ -1,6 +1,36 @@
+using System.Linq;
+using cinema.Data;
+using cinema.Models;
+
 namespace cinema.Services;
 
-public class SeatService
+public class SeatService : ISeatService
 {
-    
+    private readonly CinemaContext _context;
+
+    public SeatService(CinemaContext context)
+    {
+        _context = context;
+    }
+
+    public Array GetSeats(Show show, int quantity)
+    {
+        if (show == null || !(quantity > 0) )
+        {
+            throw new MissingFieldException();
+        }
+
+        var template = from t in _context.RoomTemplates
+            where t.Id == show.Room.Template.Id
+            select t;
+        
+        string[] response = new string[quantity];
+
+        var tickets = from t in _context.Tickets
+            select t;
+        tickets = tickets.Where(t => t.show.Id.Equals(showId));
+        
+        
+        return response;
+    }
 }
