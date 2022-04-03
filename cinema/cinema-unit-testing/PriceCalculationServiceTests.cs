c6c61a00c33c43ad3a41869de01ecd28292564c4
Sebastian Tramper <tramper.s@outlook.com>
2022-04-03 08:33:06 +0200

    Added project

--- cinema/cinema-unit-testing/PriceCalculationServiceTests.cs
+++ cinema/cinema-unit-testing/PriceCalculationServiceTests.cs
@@ -13,13 +13,8 @@ public class PriceCalculationServiceTests
     [Fact]
     public void Discount_IsCalculated_whenUserSubmits()
     {
-        var show1 = new Show();
-        var show2 = new Show();
         Mock<IShowRepository> mock = new Mock<IShowRepository>();
-        mock.Setup(s => s.FindAllIncludeMovie()).Returns(new Show[]
-        {
-            show1, show2
-        }.AsQueryable());
+      
         MovieService movieService = new MovieService(mock.Object);
         PriceCalculatingService priceCalculatingService = new PriceCalculatingService(movieService, mock.Object);
 
