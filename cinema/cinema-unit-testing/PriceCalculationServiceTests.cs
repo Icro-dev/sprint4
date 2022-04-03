c833bf0b78a506363858cb8aeeaab9c8a1f593f1
Sebastian Tramper <tramper.s@outlook.com>
2022-04-03 08:45:59 +0200

    Beter name

--- cinema/cinema-unit-testing/PriceCalculationServiceTests.cs
+++ cinema/cinema-unit-testing/PriceCalculationServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using cinema.Models;
 using cinema.Repositories;
@@ -13,10 +14,10 @@ public class PriceCalculationServiceTests
     [Fact]
     public void Discount_IsCalculated_whenUserSubmits()
     {
-        Mock<IShowRepository> mock = new Mock<IShowRepository>();
+        Mock<IShowRepository> showRepo = new Mock<IShowRepository>();
       
-        MovieService movieService = new MovieService(mock.Object);
-        PriceCalculatingService priceCalculatingService = new PriceCalculatingService(movieService, mock.Object);
+        MovieService movieService = new MovieService(showRepo.Object);
+        PriceCalculatingService priceCalculatingService = new PriceCalculatingService(movieService, showRepo.Object);
 
         double discount = priceCalculatingService.Discount(2, 2, 2);
         Assert.Equal(9.0, discount);
